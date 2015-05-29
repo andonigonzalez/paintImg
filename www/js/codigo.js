42c602489e5334ebc49e75d160d8e7ccf0915c9e
unknown <zitup3@zd03-PC.CORP.ZITU>
2015-05-29 11:52:57 +0200

    paint img

--- www/js/codigo.js
+++ www/js/codigo.js
@@ -43,6 +43,7 @@ Circle.prototype.stroke=function(ctx){
 	ctx.beginPath();
 	ctx.arc(this.x,this.y,this.radius,0,Math.PI*2,true);
 	ctx.stroke();
+	ctx.closePath();
 }
 
 Circle.prototype.distance=function(circle){
